1f501e2902344e77772fafbd4d89d97cf4e822d1
Marton <mdox@mdox.xyz>
2022-12-28 18:56:55 +0100

    Add: Fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        quicksand: ["Quicksand", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

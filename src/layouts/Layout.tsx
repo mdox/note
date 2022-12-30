1c3862dc4272c9e3e334b5542c80199df552664f
Marton <mdox@mdox.xyz>
2022-12-30 18:20:31 +0100

    Add: Signout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,9 @@
+import { mdiLogout } from "@mdi/js";
+import Icon from "@mdi/react";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import type { ReactNode } from "react";
+import Button from "../components/Button";
 
 export type LayoutProps = {
   children: ReactNode;
@@ -9,10 +13,13 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <header className="bg-gray-800 p-2">
-        <nav className="mx-auto max-w-screen-md">
+        <nav className="mx-auto flex max-w-screen-md justify-between">
           <Link href="/" className="font-quicksand text-3xl">
             CO-NOTE
           </Link>
+          <Button className="p-2" onClick={() => signOut()}>
+            <Icon path={mdiLogout} size={0.875} />
+          </Button>
         </nav>
       </header>
 
